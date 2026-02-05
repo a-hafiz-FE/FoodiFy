8e30c0bd067ad6deaebbcb8496b65ee39254d38b
a-hafiz-FE <a.hafiz@ejada.com>
2026-02-05 15:58:00 +0300

    first stage of add new screen

--- src/app/mealStore.ts
+++ src/app/mealStore.ts
@@ -1,12 +1,10 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
-import { v4 as uuid, UUIDTypes } from 'uuid';
+import { v4 as uuid } from 'uuid';
 
 type Id = string;
 type Chef = { id: Id; name: string; avatarUrl: string; rating: number };
-
 type Tag = { id: Id; label: string };
-
 type Meal = {
   id: Id;
   title: string;
@@ -175,11 +173,30 @@ type MealsSlice = {
   meals: Meal[];
   mealsById: Record<Id, Meal>;
   mealsIds: Id[];
+
   addmeal: (meal: Omit<Meal, 'id'>) => Id;
   upsertMeals: (meals: Meal[]) => void;
+
+  mealsArray: () => Meal[];
+  mealsByChef: (chefId: Id) => Meal[];
+  getMealCard: (
+    mealId: Id,
+  ) => (Meal & { chef?: Chef; tags: Tag[] }) | undefined;
 };
 
-type Store = MealsSlice;
+type ChefsSlice = {
+  chefsById: Record<Id, Chef>;
+  addChef: (chef: Omit<Chef, 'id'>) => Id;
+  upsertChefs: (chefs: Chef[]) => void;
+};
+
+type TagsSlice = {
+  tagsById: Record<Id, Tag>;
+  addTag: (tag: Omit<Tag, 'id'>) => Id;
+  upsertTags: (tags: Tag[]) => void;
+};
+
+type Store = MealsSlice & ChefsSlice & TagsSlice;
 
 const clamp = (n: number) => Math.min(5, Math.max(0, n));
 const round1 = (n: number) => Math.round(n * 10) / 10;
@@ -187,6 +204,52 @@ const round1 = (n: number) => Math.round(n * 10) / 10;
 export const useMealStore = create<Store>()(
   persist(
     (set, get) => ({
+      // ====== Tag ======
+      tagsById: {},
+
+      addTag: tag => {
+        const id = uuid();
+        const fixedtags: Tag = {
+          id,
+          ...tag,
+        };
+        set(s => ({
+          tagsById: { ...s.tagsById, [id]: fixedtags },
+        }));
+        return id;
+      },
+
+      upsertTags: tags =>
+        set(s => {
+          const next = { ...s.tagsById };
+          tags.forEach(t => (next[t.id] = t));
+          return { tagsById: next };
+        }),
+
+      // ======= Chef ========
+      chefsById: {},
+
+      addChef: chef => {
+        const id = uuid();
+        const fixedchefs: Chef = {
+          id,
+          ...chef,
+          rating: round1(clamp(chef.rating)),
+        };
+        set(s => ({
+          chefsById: { ...s.chefsById, [id]: fixedchefs },
+        }));
+        return id;
+      },
+
+      upsertChefs: chefs =>
+        set(s => {
+          const next = { ...s.chefsById };
+          chefs.forEach(c => (next[c.id] = c));
+          return { chefsById: next };
+        }),
+
+      // ======= Meal =======
       mealsById: {},
       mealsIds: [],
       meals: dummyMeals,
@@ -228,6 +291,23 @@ export const useMealStore = create<Store>()(
         get()
           .mealsIds.map(id => get().mealsById[id])
           .filter(Boolean),
+
+      mealsByChef: chefId =>
+        get()
+          .mealsIds.map(id => get().mealsById[id])
+          .filter(m => m?.chefId === chefId) as Meal[],
+
+      getMealCard: mealId => {
+        const { mealsById, chefsById, tagsById } = get();
+        const meal = mealsById[mealId];
+        if (!meal) return undefined;
+
+        return {
+          ...meal,
+          chef: chefsById[meal.chefId],
+          tags: meal.tagsIds.map(id => tagsById[id]).filter(Boolean),
+        };
+      },
     }),
     {
       name: 'storage',
--- src/screens/AddNew/AddNewScreen.tsx
+++ src/screens/AddNew/AddNewScreen.tsx
@@ -1,18 +1,219 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import { styles } from '../Home/styles';
+import { useFocusEffect } from '@react-navigation/native';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { Image, Pressable, Text, View } from 'react-native';
+import {
+  launchImageLibrary,
+  type ImageLibraryOptions,
+  type PhotoQuality,
+} from 'react-native-image-picker';
 
 const AddNewScreen = () => {
+  const [step, setStep] = useState(1);
+  const [selectedImage, setSelectedImage] = useState('');
+  const totalSteps = 5;
+
+  const handleNext = () => {
+    setStep(p => Math.min(p + 1, totalSteps));
+  };
+
+  const handelPrev = () => {
+    setStep(p => Math.max(p - 1, 1));
+  };
+
+  const options: ImageLibraryOptions = {
+    mediaType: 'photo',
+    selectionLimit: 1,
+    quality: 80 as PhotoQuality,
+  };
+
+  const titles = [
+    'Select Photo',
+    'Recipe Information',
+    'Ingredients',
+    'Introduction',
+    'Recipe Information',
+  ];
+
+  const openedOnce = useRef(false);
+
+  useFocusEffect(
+    useCallback(() => {
+      if (!openedOnce.current) {
+        openedOnce.current = true;
+
+        const t = setTimeout(async () => {
+          const res = await launchImageLibrary(options);
+
+          if (res.didCancel) {
+            console.log('User cancelled image picker');
+            return;
+          }
+
+          if (res.errorCode) {
+            console.log('Image picker error', res.errorMessage);
+            return;
+          }
+
+          const uri = res.assets?.[0]?.uri;
+          if (!uri) {
+            console.log('No image uri returned');
+            return;
+          }
+          setSelectedImage(uri);
+        }, 200);
+
+        return () => clearTimeout(t);
+      }
+      return () => {
+        openedOnce.current = false;
+      };
+    }, []),
+  );
+
+  const renderStepsIndecator = () => {
+    const indecator = [];
+    for (let i = 1; i <= totalSteps; i++) {
+      indecator.push(
+        <View
+          key={i}
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: '#353535',
+              height: 32,
+              width: i === step ? 'auto' : 32,
+              paddingHorizontal: 12,
+              borderRadius: 99,
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderCurve: 'continuous',
+            }}
+          >
+            <Text style={{ color: i <= step ? '#DEE21B' : '#fff' }}>
+              {i === step ? <Text>{titles[i - 1]}</Text> : i}
+            </Text>
+          </View>
+          {i < totalSteps && (
+            <View
+              style={{
+                backgroundColor: '#353535',
+                height: 6,
+                width: 8,
+                borderCurve: 'continuous',
+              }}
+            />
+          )}
+        </View>,
+      );
+    }
+    return (
+      <View style={{ flexDirection: 'row', marginTop: 70 }}>{indecator}</View>
+    );
+  };
   return (
     <View
       style={{
-        backgroundColor: '#ffffff',
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
       }}
     >
-      <Text style={styles.text}>Add New Screen</Text>
+      {renderStepsIndecator()}
+
+      <View style={{ flex: 1, marginVertical: 25 }}>
+        {step === 1 && (
+          <View>
+            <Text>Step 1</Text>
+            {selectedImage && (
+              <Image
+                source={{ uri: selectedImage }}
+                style={{ width: 200, height: 200 }}
+              />
+            )}
+          </View>
+        )}
+        {step === 2 && (
+          <View>
+            <Text>Step 2</Text>
+          </View>
+        )}
+        {step === 3 && (
+          <View>
+            <Text>Step 3</Text>
+          </View>
+        )}
+        {step === 4 && (
+          <View>
+            <Text>Step 4</Text>
+          </View>
+        )}
+        {step === 5 && (
+          <View>
+            <Text>Step 5</Text>
+          </View>
+        )}
+      </View>
+      <View
+        style={{
+          position: 'absolute',
+          bottom: 30,
+          flexDirection: 'row',
+          gap: 20,
+        }}
+      >
+        {step > 1 && (
+          <Pressable
+            onPress={handelPrev}
+            style={{
+              borderRadius: 12,
+              borderWidth: 1,
+              width: 128,
+              height: 40,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <Text>Previous</Text>
+          </Pressable>
+        )}
+        {step < totalSteps ? (
+          <Pressable
+            onPress={handleNext}
+            style={{
+              borderRadius: 12,
+              borderWidth: 1,
+              width: 128,
+              height: 40,
+              alignItems: 'center',
+              justifyContent: 'center',
+              position: step === 1 ? 'absolute' : 'relative',
+              left: step === 1 ? 10.5 : 'auto',
+              bottom: step === 1 ? 0 : 'auto',
+              backgroundColor: '#353535',
+            }}
+          >
+            <Text style={{ color: '#fff' }}>Next</Text>
+          </Pressable>
+        ) : (
+          <Pressable
+            onPress={handleNext}
+            style={{
+              borderRadius: 12,
+              borderWidth: 1,
+              width: 128,
+              height: 40,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: '#353535',
+            }}
+          >
+            <Text style={{ color: '#fff' }}>Finish</Text>
+          </Pressable>
+        )}
+      </View>
     </View>
   );
 };
